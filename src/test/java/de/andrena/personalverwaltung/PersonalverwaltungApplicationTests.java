7c951967222845a1a7c6278ee3d566921bf35dec
Jan Heffner <jan.heffner@andrena.de>
2021-10-19 16:48:59 +0200

    inital

---
+++ src/test/java/de/andrena/personalverwaltung/PersonalverwaltungApplicationTests.java
@@ -0,0 +1,13 @@
+package de.andrena.personalverwaltung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalverwaltungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
